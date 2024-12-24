0f228d963ba46cd66a7f82efd4d5c2bda69804cc
Tod9197 <dancein18@yahoo.co.jp>
2024-12-24 21:49:08 +0900

    class追記

--- training/class2.ts
+++ training/class2.ts
@@ -1,23 +1,66 @@
-class Fraction2 {
+// class Fraction2 {
+//   constructor(private _numerator: number, private _denominator: number) {}
+
+//   get numerator() {
+//     return this._numerator;
+//   }
+
+//   get denominator() {
+//     return this._denominator;
+//   }
+//   getValue(): number {
+//     return this._denominator / this._numerator;
+//   }
+//   toStoring(): string {
+//     return `${this._numerator}/${this._denominator}`;
+//   }
+// }
+
+// const e1 = new Fraction2(5, 15);
+// console.log(e1.numerator);
+// console.log(e1.denominator);
+// console.log(e1.getValue());
+// console.log(e1.toStoring());
+
+class Fraction3 {
+  numerator: number;
+  denominator: number;
+
+  constructor(numertor: number, denominator: number) {
+    this.numerator = numertor;
+    this.denominator = denominator;
+  }
+
+  getValue() {
+    return this.numerator / this.denominator;
+  }
+}
+
+const g1 = new Fraction3(72, 244);
+console.log(g1.numerator);
+console.log(g1.denominator);
+console.log(g1.getValue());
+
+class Fraction4 {
   constructor(private _numerator: number, private _denominator: number) {}
 
   get numerator() {
     return this._numerator;
   }
-
   get denominator() {
     return this._denominator;
   }
-  getValue(): number {
-    return this._denominator / this._numerator;
-  }
-  toStoring(): string {
+
+  toString(): string {
     return `${this._numerator}/${this._denominator}`;
+  }
+  getVlaue(): number {
+    return this._numerator / this._denominator;
   }
 }
 
-const e1 = new Fraction2(5, 15);
-console.log(e1.numerator);
-console.log(e1.denominator);
-console.log(e1.getValue());
-console.log(e1.toStoring());
+const k1 = new Fraction4(8, 9);
+console.log(k1.numerator);
+console.log(k1.denominator);
+console.log(k1.toString());
+console.log(k1.getVlaue());
