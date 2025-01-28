43ae6467cb9ce523c4ab36eff5d699e18e5b9485
Tod9197 <dancein18@yahoo.co.jp>
2025-01-28 19:24:33 +0900

    番兵の実装

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,9 +1,13 @@
-import { Disc } from "./disc";
+import { Disc, isOppositeDisc } from "./disc";
 import { Move } from "./move";
 import { Point } from "./point";
 
 export class Board {
-  constructor(private _discs: Disc[][]) {}
+  private _wallDiscs: Disc[][];
+
+  constructor(private _discs: Disc[][]) {
+    this._wallDiscs = this.wallDiscs();
+  }
 
   place(move: Move): Board {
     // 空のマス目ではない場合、置くことはできない
@@ -12,7 +16,7 @@ export class Board {
     }
 
     // ひっくり返せる点をリストアップ
-    const flipPoints = this.listFlipPoints();
+    const flipPoints = this.listFlipPoints(move);
 
     // ひっくり返せる点がない場合、置くことはできない
     if (flipPoints.length === 0) {
@@ -32,8 +36,50 @@ export class Board {
     return new Board(newDiscs);
   }
 
-  private listFlipPoints(): Point[] {
-    return [new Point(0, 0)];
+  private listFlipPoints(move: Move): Point[] {
+    const flipPoints: Point[] = [];
+
+    const walldX = move.point.x + 1;
+    const walledY = move.point.y + 1;
+
+    // 上
+    const flipCandidate: Point[] = [];
+
+    // 1つ動いた位置から開始
+    let cursorX = walldX;
+    let cursorY = walledY - 1;
+
+    // 手と逆の色の石がある間、1つずつ見ていく
+    while (isOppositeDisc(move.disc, this._wallDiscs[cursorY][cursorX])) {
+      // 番兵を考慮して-1する
+      flipCandidate.push(new Point(cursorX - 1, cursorY - 1));
+      cursorY--;
+      // 次の手が同じ色の石なら、ひっくり返す石が確定
+      if (move.disc === this._wallDiscs[cursorY][cursorX]) {
+        flipPoints.push(...flipCandidate);
+        break;
+      }
+    }
+
+    return flipPoints;
+  }
+
+  private wallDiscs(): Disc[][] {
+    const walled: Disc[][] = [];
+
+    const topAndBottomWall = Array(this._discs[0].length + 2).fill(Disc.Wall);
+
+    walled.push(topAndBottomWall);
+
+    this._discs.forEach((line) => {
+      const walledLine = [Disc.Wall, ...line, Disc.Wall];
+      walled.push(walledLine);
+    });
+
+    //
+    walled.push(topAndBottomWall);
+
+    return walled;
   }
 
   get discs() {
